34945e507bf863cd34e5ecca1f1ee7180ab8e6ff
zhi <RodgerLZ@163.com>
2020-04-16 19:41:44 +0800

    28.实现 strStr()

--- src/字符串/L028.java
+++ src/字符串/L028.java
@@ -4,6 +4,10 @@ package 字符串;
  * 28.实现 strStr()
  */
 public class L028 {
+
+    /**
+     * 双指针算法
+     */
     public int strStr(String haystack, String needle) {
 
         if (needle == null || needle.isEmpty()) return 0;
@@ -25,5 +29,54 @@ public class L028 {
         }
 
         return -1;
+    }
+
+    /**
+     * Rabin Karp 算法
+     * 通过滑动窗口 + 自定义hashcode的算法，时间复杂度为o(n)
+     */
+    public static int strStr01(String haystack, String needle) {
+
+        // 所需查找的目标字符串为空时
+        if (needle==null || needle.length()==0) return 0;
+
+        int sLen = haystack.length();
+        int tLen = needle.length();
+
+        if (sLen < tLen) return -1;
+
+        // 计算hashCode取模时用，防止溢出
+        long modulus = (long) Math.pow(2, 31);
+        // 用于计算hash值，这里题目中有26个英文字母，所以选26
+        int a = 26;
+
+        // 计算haystack和needle的hash值
+        long sHa = 0, tHa = 0;
+        for (int i=0; i<tLen; i++) {
+            sHa = (sHa * a + charToInt(haystack, i)) % modulus;
+            tHa = (tHa * a + charToInt(needle, i)) % modulus;
+        }
+
+        if (sHa == tHa) return 0;
+
+        // 求出a**tLen % modulus
+        long aTLen = 1;
+        for (int i=1; i<=tLen; i++) aTLen = aTLen * a % modulus;
+
+        // 每次向后滑动一个字符，计算滑动后窗口对应的字符串的hashCode
+        for (int i=1; i<sLen-tLen+1; i++) {
+            sHa = (sHa * a - charToInt(haystack, i-1)*aTLen + charToInt(haystack, i+tLen-1)) % modulus;
+            if (sHa == tHa) return i;
+        }
+
+        return -1;
+    }
+
+    private static int charToInt(String str, int index) {
+        return str.charAt(index) - 'a';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr01("a","a"));
     }
 }
