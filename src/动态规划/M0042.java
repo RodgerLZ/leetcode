a1e86c769a6f183fa045567174a5508301057f2b
zhi <RodgerLZ@163.com>
2020-06-01 23:44:30 +0800

    42. 连续子数组的最大和

---
+++ src/动态规划/M0042.java
@@ -0,0 +1,22 @@
+package 动态规划;
+
+public class M0042 {
+    public static int maxSubArray(int[] nums) {
+
+        if (nums.length == 0) return 0;
+
+        int[] dp = new int[nums.length];
+
+        int max = nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i<nums.length; i++) {
+            dp[i] = nums[i] + Math.max(0, dp[i-1]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+}
