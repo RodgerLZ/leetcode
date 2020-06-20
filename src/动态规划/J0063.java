88b91dcf0905169878231595d845fcb67059b3d5
zhi <RodgerLZ@163.com>
2020-06-20 10:38:21 +0800

    63. 不同路径 II

---
+++ src/动态规划/J0063.java
@@ -0,0 +1,27 @@
+package 动态规划;
+
+/**
+ * 63. 不同路径 II
+ */
+public class J0063 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        if (obstacleGrid.length==0
+                || obstacleGrid[0].length==0
+                || obstacleGrid[0][0] == 1) return 0;
+
+        int n = obstacleGrid[0].length;
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        for (int[] ints : obstacleGrid) {
+            for (int j = 0; j < n; j++) {
+                if (ints[j] == 1) dp[j] = 0;
+                else if (j != 0) dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp[n-1];
+    }
+}
