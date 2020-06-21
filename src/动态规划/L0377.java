863386366a871310f0c5b72efe00e3e7d80efc53
zhi <RodgerLZ@163.com>
2020-06-21 22:16:57 +0800

    377. 组合总和 Ⅳ

--- src/动态规划/L0377.java
+++ src/动态规划/L0377.java
@@ -4,6 +4,8 @@ package 动态规划;
  * 377. 组合总和 Ⅳ
  */
 public class L0377 {
+
+    // todo 如果顺序不同的序列视为相同的组合，这题需要怎么解。为什么？
     public int combinationSum4(int[] nums, int target) {
 
         int[] dp = new int[target + 1];
