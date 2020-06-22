91fd3fbf59ce9e9f81bae1deb191edb13e49c1e7
zhi <RodgerLZ@163.com>
2020-06-22 23:08:44 +0800

    122. 买卖股票的最佳时机 II

--- src/动态规划/L0122.java
+++ src/动态规划/L0122.java
@@ -19,6 +19,28 @@ public class L0122 {
         }
 
         return res;
+    }
 
+
+    // 动态规划
+    // 这题更适合用贪心算法，动态规划只为了掌握方法。
+    public int maxProfitDp(int[] prices) {
+
+        int len = prices.length;
+        if(len<2) return 0;
+
+        int cash = 0;
+        int stock = -prices[0];
+        int preCash;
+        int preStock;
+
+        for (int i=1; i<len; i++) {
+            preCash = cash;
+            preStock = stock;
+            cash = Math.max(preCash, preStock + prices[i]);
+            stock = Math.max(preCash - prices[i], preStock);
+        }
+
+        return cash;
     }
 }
